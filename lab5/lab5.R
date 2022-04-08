1431bd602c1eac309307069f0ec9102a27d4e708
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-04-08 12:56:03 +0300

    Лаба 5 задание 4

---
+++ lab5/lab5.R
@@ -0,0 +1,16 @@
+install.packages("rvest")
+library(rvest)
+
+# Считывание всех музеев со страницы
+url_museum <- read_html("https://tonkosti.ru/Музеи_Санкт-Петербурга") # Считали ссылку
+
+select_museum <- "places-list__item-header" # Название класса хранящий имя музея
+select_museum_address <- "div.places-list__address--rc"# Класс хранящий адреса музеев
+select_museum_url <- ".places-list__item-img--rc"
+
+museums_names <- html_nodes(url_museum, select_museum) %>% html_text() %>% as.array() # Считали названия всех музеев
+museums_adress <- html_nodes(url_museum, select_museum_address) %>% html_text() %>% as.array() # Считали адреса всех музеев
+museums_urls <- html_nodes(url_museum, select_museum_url) %>% html_text("href") %>% as.array()
+
+museums_names
+museums_adress
