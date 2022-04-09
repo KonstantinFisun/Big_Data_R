5dd5ef0b4afd75c344e472810370ab3171ed2e03
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-04-09 21:45:40 +0300

    Удалил повторения в адресах

--- lab5/lab5.R
+++ lab5/lab5.R
@@ -6,12 +6,13 @@ url_museum <- read_html("https://tonkosti.ru/Музеи_Санкт-Петербурга") # Считали 
 
 select_museum <- "h3.places-list__item-header" # Название класса хранящий имя музея
 select_museum_address <- "div.places-list__address--rc"# Класс хранящий адреса музеев
-select_museum_url <- "a.places-list__item-img"
+select_museum_url <- ".places-list__item-img"
 
 museums_names <- html_nodes(url_museum, select_museum) %>% html_text() %>% as.array() # Считали названия всех музеев
 museums_adress <- html_nodes(url_museum, select_museum_address) %>% html_text() %>% as.array() # Считали адреса всех музеев
-museums_urls <- html_nodes(url_museum, select_museum_url) %>% html_text("href") %>% as.array()
+museums_urls <- html_nodes(url_museum, select_museum_url) %>% html_attr("href") %>% as.array()
 
 museums_names
 museums_adress
+museums_urls <- unique(museums_urls) # Удалили повторения
 
