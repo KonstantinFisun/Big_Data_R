9c89e550325dc078c64eef6739af2fbaf29de81a
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-04-18 23:30:37 +0300

    По тупому считывание страниц

--- lab5/lab5.R
+++ lab5/lab5.R
@@ -1,17 +1,35 @@
 install.packages("rvest")
 library(rvest)
 
-url_country_rating <- read_html("https://www.numbeo.com/quality-of-life/rankings_by_country.jsp?title=2021")
+# url_country_rating <- read_html("https://www.numbeo.com/quality-of-life/rankings_by_country.jsp?title=2021")
 
 # select_country <- ".cityOrCountryInIndicesTable" # Название класса хранящий имена стран
+# select_rat <- "td" # Таблица рейтинга
+
+# country_names <- html_nodes(url_country_rating, select_country) %>% html_text() %>% as.array() # Считали имена стран с таблицы
+# country_rat <- html_nodes(url_country_rating, select_rat) %>% html_text() %>% as.array() # Считали рейтинги
+# country_rat <- country_rat[-c(1,2,3)] # Убрали лишние из вектора
+# contry_rat <- matrix(country_rat, ncol = 11, nrow = 83, byrow = TRUE) # Создание матрицы
+
 select_rat <- "td" # Таблица рейтинга
 
-#country_names <- html_nodes(url_country_rating, select_country) %>% html_text() %>% as.array() # Считали имена стран с таблицы
-country_rat <- html_nodes(url_country_rating, select_rat) %>% html_text() %>% as.array() # Считали рейтинги
-country_rat <- country_rat[-c(1,2,3)] # Убрали лишние из вектора
-contry_rat <- matrix(country_rat, ncol = 11, nrow = 83, byrow = TRUE) # Создание матрицы
+# Ссылки на определенные года
+url_country_rating_2022 <- read_html("https://www.numbeo.com/quality-of-life/rankings_by_country.jsp?title=2022") # 22
+url_country_rating_2021 <- read_html("https://www.numbeo.com/quality-of-life/rankings_by_country.jsp?title=2021") # 21
+url_country_rating_2020 <- read_html("https://www.numbeo.com/quality-of-life/rankings_by_country.jsp?title=2020") # 20
+url_country_rating_2019 <- read_html("https://www.numbeo.com/quality-of-life/rankings_by_country.jsp?title=2019") # 19
+url_country_rating_2018 <- read_html("https://www.numbeo.com/quality-of-life/rankings_by_country.jsp?title=2018") # 18
 
-contry_rat <- contry_rat[,-c(11)] # Удаление пустого столбца
+
+all_url = c(url_country_rating_2022,url_country_rating_2021,url_country_rating_2020,url_country_rating_2022,url_country_rating_2022)
+
+country_rat_2022 <- html_nodes(url_country_rating_2022, select_rat) %>% html_text() %>% as.array() # Считали рейтинги
+country_rat_2022 <- country_rat[-c(1,2,3)] # Убрали лишние из вектора
+contry_rat_2022 <- matrix(country_rat, ncol = 11, nrow = 83, byrow = TRUE) # Создание матрицы
+
+
+
+contry_rat_20 <- contry_rat[,-c(11)] # Удаление пустого столбца
 
 # Создание базы
 rating <- data.frame(contry_rat)
@@ -21,9 +39,16 @@ names(rating) <- c("Страна", "Индекс качества жизни", "Индекс покупательной спос
                               "Индекс времени в пути в пробках", "Индекс загрезнения", "Климатический индекс")
 
 
+
+
 # Канада, США, Турция, Греция, Дания - мои страны
 # Составим таблицу из моих стран
 rating_my <- rating[c(2,15,20,43,45),]
+
+
+# Сравнение стран по Индексу жизни за последнии 5 лет
+
+
 
 # Задание с музеем
 # Считывание всех музеев со страницы
