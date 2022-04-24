ce234aa989b99f86f3e32b8be2408ffe4981d560
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-04-24 12:21:41 +0300

    Жалкие попытки

--- lab6/lab6.R
+++ lab6/lab6.R
@@ -14,3 +14,31 @@ airplan_crushes[airplan_crushes == ""] <- NA # Пустые значение, с
 
 # Удалим строки, где есть пустые значения
 airplan_crushes <- airplan_crushes[rowSums(is.na(airplan_crushes[,])) == 0,]
+
+# Будем определять пассажирские или военный самолет по числу на борте
+
+# Будем определять самолеты, на которых больше шанс выжить при крушении
+
+# Стандартизация переменных
+
+airplane_N <- airplan_crushes[,c(10,11)]
+
+maxs <- apply(airplane_N, 2, max) # Максимальное значение
+mins <- apply(airplane_N, 2, min) # Минимальное значение
+
+airplane_N <- scale(airplane_N, center = mins, scale = maxs - mins)
+
+# Матрица попарных расстояний 
+dist.airplan_crushes <- dist(airplane_N)
+
+# Проводим кластерный анализ, результаты записываем в список clust.iris
+clust.airplan_crushes <- hclust(dist.airplan_crushes, "ward.D")
+
+# Построение дендрограммы
+plot(clust.airplan_crushes)
+
+labels <- airplan_crushes[,4]
+
+# целесообразно разделить ее на 3 либо на 4 кластера. Попробуем разделить на 4 кластера.
+plot(clust.airplan_crushes, labels, cex=0.5)
+rect.hclust(clust.airplan_crushes, k=3, border="red")
