c2a804d05bad672560fb56bd99db2236d7d6b9ce
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-04-24 11:53:37 +0300

    Считывание dataset и убираем пропуски в данных

---
+++ lab6/lab6.R
@@ -0,0 +1,16 @@
+library("xlsx")
+airplan_crushes <- read.csv('C:/Users/kosty/OneDrive/Документы/GitHub/Big_Data_R/lab6/Airplane_Crashes.csv', sep = ",") # Считали базу данных
+
+names_cols_airplan_crushes <- c("Дата", "Время", "Место крушения", "Компания",
+               "Полет", "Рейс", "Самолет", "Бортовой номер",
+               "Заводской серийный номер", "Находилось на борту", 
+               "Cмертельные случаи на борту", "Убитые при приземлении",
+               "Описание крушения") # Название столбцов
+
+colnames(airplan_crushes) <- names_cols_airplan_crushes # Присвоили имена столбикам
+
+airplan_crushes[airplan_crushes == ""] <- NA # Пустые значение, сделали NA
+
+
+# Удалим строки, где есть пустые значения
+airplan_crushes <- airplan_crushes[rowSums(is.na(airplan_crushes[,])) == 0,]
