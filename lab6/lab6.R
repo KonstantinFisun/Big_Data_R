9c1b732512114dd1a11e5b809f0f8f2f4902119b
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-05-14 23:11:16 +0300

    Дендрограмма

--- lab6/lab6.R
+++ lab6/lab6.R
@@ -1,4 +1,5 @@
 library("xlsx")
+
 airplan_crushes <- read.csv('C:/Users/kosty/OneDrive/Документы/GitHub/Big_Data_R/lab6/Airplane_Crashes.csv', sep = ",") # Считали базу данных
 
 names_cols_airplan_crushes <- c("Дата", "Время", "Место крушения", "Компания",
@@ -15,13 +16,11 @@ airplan_crushes[airplan_crushes == ""] <- NA # Пустые значение, с
 # Удалим строки, где есть пустые значения
 airplan_crushes <- airplan_crushes[rowSums(is.na(airplan_crushes[,])) == 0,]
 
-# Будем определять пассажирские или военный самолет по числу на борте
-
-# Будем определять самолеты, на которых больше шанс выжить при крушении
+# Оставили 100 рейсов
+airplan_crushes <- airplan_crushes[800:944,]
 
 # Стандартизация переменных
-
-airplane_N <- airplan_crushes[,c(10,11)]
+airplane_N <- airplan_crushes[,c(10,11,12)]
 
 maxs <- apply(airplane_N, 2, max) # Максимальное значение
 mins <- apply(airplane_N, 2, min) # Минимальное значение
@@ -31,14 +30,17 @@ airplane_N <- scale(airplane_N, center = mins, scale = maxs - mins)
 # Матрица попарных расстояний 
 dist.airplan_crushes <- dist(airplane_N)
 
-# Проводим кластерный анализ, результаты записываем в список clust.iris
+# Проводим кластерный анализ, результаты записываем в список clust.airplan_crushes
 clust.airplan_crushes <- hclust(dist.airplan_crushes, "ward.D")
 
-# Построение дендрограммы
-plot(clust.airplan_crushes)
+labels <- airplan_crushes[,7]
 
-labels <- airplan_crushes[,4]
+# Дендрограмма
+plot(clust.airplan_crushes, labels, cex=0.5, main="Дендрограмма кластеров",
+     xlab="Самолеты")
 
-# целесообразно разделить ее на 3 либо на 4 кластера. Попробуем разделить на 4 кластера.
-plot(clust.airplan_crushes, labels, cex=0.5)
-rect.hclust(clust.airplan_crushes, k=6, border="red")
+# целесообразно разделить ее на 3 кластера.
+rect.hclust(clust.airplan_crushes, k=3, border="red")
+
+
+
