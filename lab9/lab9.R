368330c3327dd2bc7bea497a8c95ea2aeea786c1
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-05-25 16:10:24 +0300

    Команды на графы

--- lab9/lab9.R
+++ lab9/lab9.R
@@ -1,4 +1,39 @@
 library("igraph")
 library("network")
 library("sna")
-library("ndtv")
+
+# Создание графа
+g1<-graph(c(1,2,1,3,1,4,2,3,2,4, 3,4, 4,5, 5,6, 2,6),n=6)
+
+# Вывод вершин
+V(g1)
+
+# Вывод ребер
+E(g1)
+E(g1, P=NULL, path=NULL, directed=TRUE)
+
+# Добавление новых вершин и ребер в граф: 
+add.edges(g1, edges, ..., attr=list())
+add.vertices(g1, vertices, ..., attr=list())
+
+# Для создания полносвязного направленного/ненаправленного графа с циклами/без циклов 
+# используйте команду graph.full с дополнительными параметрами
+g<-graph.full(n=10, directed = FALSE, loops = FALSE)
+
+# Граф звезда
+g<-graph.star(n=10, mode="out")
+
+g<-graph.star(n=10, mode="in")
+
+#	Граф-кольцо
+g<-graph.ring(n=10)
+
+#	Граф с заданным списком ребер
+edges <- c(1,2, 3,2, 2,4)
+g<-graph(edges, n=max(edges), directed=TRUE)
+
+# Отрисовка графа
+plot(g1)
+# Матрица смежности
+g1[]
+
